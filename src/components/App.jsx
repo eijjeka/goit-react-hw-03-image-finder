46d4700d3ec22a8cad7e5bc9b3a0b757d3af12f1
eijjeka <eijjeka@gmail.com>
2022-05-07 00:13:10 +0300

    homework done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,30 @@
-function App() {
-  return <>Hello World!</>;
+import React, { Component } from "react";
+import Form from "./Form";
+import SearchInfo from "./SearchInfo";
+import { ToastContainer } from "react-toastify";
+
+class App extends Component {
+  state = {
+    imageName: "",
+  };
+
+  handleFormSubmit = (inputValueName) => {
+    this.setState({
+      imageName: inputValueName,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Form onFormSubmit={this.handleFormSubmit} />
+        <div className="container">
+          <SearchInfo imageName={this.state.imageName} />
+        </div>
+        <ToastContainer autoClose={3000} />
+      </>
+    );
+  }
 }
 
 export default App;
